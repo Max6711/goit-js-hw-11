9fae548d7d13432b8783a7946cd3887826b23e24
Max6711 <86676971+Max6711@users.noreply.github.com>
2024-05-20 08:29:56 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,27 +1,12 @@
-const loader = document.querySelector('.loader');
+import axios from 'axios';
 
-function fetchImg(searchInput) {
-  const apiKey = '42869495-3eaffa1d7f59c13a6a9af4ac7';
-  const searchWord = searchInput.value;
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchWord}&orientation=horizontal&safesearch=true`;
-  showLoader();
-  if (searchWord === '') {
-    return;
-  }
+const API_KEY = '43943872-0cd26c97fed3a84845e31fcef';
 
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
-
-export default fetchImg;
-
-function showLoader() {
-  loader.classList.add('is-open');
-}
-function hideLoader() {
-  loader.classList.remove('is-open');
+export function fetchImages(query) {
+  return axios
+    .get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+    .then(response => response.data.hits)
+    .catch(error => console.error(error));
 }
