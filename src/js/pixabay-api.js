b68da791bf2fa24d444a28ea1edb9149500f8959
Max6711 <86676971+Max6711@users.noreply.github.com>
2024-05-21 09:53:10 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -8,5 +8,4 @@ export function fetchImages(query) {
       `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
     )
     .then(response => response.data.hits)
-    .catch(error => console.error(error));
-}
+    .catch(error => console.error(error));}
