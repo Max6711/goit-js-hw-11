54f7337b89e2d6575fd2237a7322ad95a17442c9
Max6711 <86676971+Max6711@users.noreply.github.com>
2024-05-22 19:56:43 +0300

    1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,43 +1,23 @@
-import SimpleLightbox from "simplelightbox";
-
-export const listImg = document.querySelector('.list');
-let lightbox;
-
-export const markupInterface = (data) => {
-    const markup = data.hits.map((hit) => {
-    return `
-      <li class="item-list">
-        <a href="${hit.largeImageURL}" class="item-list-link">
-            <img class="item-list-img" src="${hit.webformatURL}" alt="${hit.tags}">
-        </a>
-        <div class='markup-info'>
-            <div class="item-list-info-text">
-                <h3 class="item-list-title">Likes</h3>
-                <p class="item-list-text">${hit.likes}</p>
+export const createGalleryItemMarkup = images => {
+  return images
+    .map(
+      ({ webformatURL, largeImageURL, tags, likes }) => `
+        <div class="col">
+          <div class="card shadow-sm">
+            <a href="${largeImageURL}" class="gallery-link">
+              <img src="${webformatURL}" alt="${tags}" class="gallery-img img-fluid" />
+            </a>
+            <div class="card-body">
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">Likes: ${likes}</small>
+              </div>
             </div>
-            <div class="item-list-info-text">
-                <h3 class="item-list-title">Views</h3>
-                <p class="item-list-text">${hit.views}</p>
-            </div>
-            <div class="item-list-info-text">
-                <h3 class="item-list-title">Comments</h3>
-                <p class="item-list-text">${hit.comments}</p>
-            </div>
-            <div class="item-list-info-text">
-                <h3 class="item-list-title">Downloads</h3>
-                <p class="item-list-text">${hit.downloads}</p>
-            </div>
-        </div>
-      </li>
-    `;
-}).join("");
-listImg.innerHTML = markup;
-
-lightbox = new SimpleLightbox('.item-list-link', {
-    captionsData: 'alt', 
-    captionDelay: 250 ,
-    overlayOpacity: 0.8,
-  });
-
-lightbox.refresh();
+          </div>
+        </div>`
+    )
+    .join('');
 };
